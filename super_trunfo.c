83c48f1d5b43ab556c5ae318152e0eba30ca5a86
Danilo Jesus <hammerdevofc@gmail.com>
2025-06-17 21:50:14 -0300

    Nível Aventureiro

--- super_trunfo.c
+++ super_trunfo.c
@@ -9,6 +9,8 @@ int main() {
     float area1;
     float pib1;
     int pontos1;
+    float densidade1;
+    float pib_per_capita1;
 
     // Carta 2
     char estado2;
@@ -18,6 +20,8 @@ int main() {
     float area2;
     float pib2;
     int pontos2;
+    float densidade2;
+    float pib_per_capita2;
 
     // Entrada carta 1
     printf("Configurando a carta de número 1:\n");
@@ -42,6 +46,9 @@ int main() {
 
     printf("Número de Pontos Turísticos: ");
     scanf("%d", &pontos1);
+
+    densidade1 = populacao1 / area1;
+    pib_per_capita1 = pib1 / populacao1;
 
     printf("\n");
 
@@ -69,6 +76,9 @@ int main() {
     printf("Número de Pontos Turísticos: ");
     scanf("%d", &pontos2);
 
+    densidade2 = populacao2 / area2;
+    pib_per_capita2 = pib2 / populacao2;
+
     printf("\n");
 
     // Saída carta 1
@@ -80,6 +90,9 @@ int main() {
     printf("Área: %.2f km²\n", area1);
     printf("PIB: %.2f bilhões de reais\n", pib1);
     printf("Número de Pontos Turísticos: %d\n", pontos1);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade1);
+    printf("PIB per Capita: %.8f bilhões de reais\n", pib_per_capita1); // 8 casas pra não sumir
+
     printf("\n");
 
     // Saída carta 2
@@ -91,7 +104,8 @@ int main() {
     printf("Área: %.2f km²\n", area2);
     printf("PIB: %.2f bilhões de reais\n", pib2);
     printf("Número de Pontos Turísticos: %d\n", pontos2);
-    printf("\n");
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade2);
+    printf("PIB per Capita: %.8f bilhões de reais\n", pib_per_capita2);
 
     return 0;
 }
